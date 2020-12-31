b6357f178f5b64f34f572ce8b2518babd8eb325f
PawaniPubudika <pubudikalwp@gmail.com>
2020-12-31 10:43:02 +0530

    Create write post modal

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,16 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component}  from 'react';
+import {useState} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap';
 import { Form } from 'react-bootstrap';
+import Modal from 'react-modal';
 
-class Blog extends Component{
-    render() {
+
+function Blog(){
+        const[modalIsOpen,setModalIsOpen]=useState(false)
         return (
             <div className="container">
                 <div>
                     <Card  border="primary">
                         <CardImg top width="100%" height="450px" src="assets/images/img1.jpg" alt="Home Card" />
                         <CardBody>
-                        <CardTitle tag="h5">First Blog Post</CardTitle>
+                        <CardTitle tag="h5">Blog Post</CardTitle>
                             <CardSubtitle tag="h6" className="mb-2 text-muted"> <img src="assets/images/user.png" name="hasintha" alt="hasintha" width="32px" height="32px"/>   Writtern by: Hasintha Lahiru</CardSubtitle>
                             <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -94,7 +97,45 @@ class Blog extends Component{
                             <CardBody>
                                 <CardTitle tag="h5">Write a blog post</CardTitle>
                                 <CardSubtitle tag="h6" className="mb-2 text-muted">Here you can write blog posts for CSUP blogpage</CardSubtitle>
-                                <Button>Write Post</Button> 
+                                <div>
+                                <button onClick={()=>setModalIsOpen(true)}>Write Post</button>
+                                    <Modal isOpen={modalIsOpen} onRequestClose={()=>setModalIsOpen(false)} 
+                                            style={
+                                                    {
+                                             overlay:{
+                                                    backgroundColor:'grey'},
+                                             content:{
+                                                    color:'black',
+                                                    fontSize:'140%',}
+                                                }}>
+        
+                                    <h2>Write a Blog Post</h2>
+                                    <Form>
+                                    <Form.Group >
+                                    <Form.Group controlId="formGridAddress1">
+                                    <Form.Label>Topic</Form.Label>
+                                    <Form.Control placeholder="Add topic" />
+                                    </Form.Group>
+                                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                                    <Form.Label>Content</Form.Label>
+                                    <Form.Control placeholder="Write your Post" as="textarea" rows={3} />
+                                    </Form.Group>
+                                    </Form.Group>
+                                    <Form>
+                                    <Form.Group>
+                                    <Form.File id="exampleFormControlFile1" label="Upload Image" />
+                                    </Form.Group>
+                                    </Form>
+                                    <Button variant="primary" type="submit">
+                                    Submit
+                                    </Button>
+                                    </Form>
+                                    <br/>        
+                                    <div>
+                                    <Button onClick={()=>setModalIsOpen(false)}>close</Button>
+                                    </div>
+                                    </Modal>
+                                </div> 
                             </CardBody>
                         </Card>
                     </div>
@@ -102,6 +143,6 @@ class Blog extends Component{
             </div>
         );
     }
-}
+
 
 export default Blog;
