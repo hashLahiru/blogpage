a884e1d2e3ce3c915359f6ba529a631f2b0e939a
hashLahiru <hasinthalahiru333@gmail.com>
2021-01-17 14:23:56 +0530

    LastOne 2021.17.01

--- FrontEnd/src/components/BlogComponent.js
+++ FrontEnd/src/components/BlogComponent.js
@@ -1,4 +1,4 @@
-import React, {Component}  from 'react';
+import React  from 'react';
 import {useState} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap';
 import { Form } from 'react-bootstrap';
@@ -48,48 +48,7 @@ function Blog(){
                     <br />
                 </div>
                 
-                <div className="row align-items-start">
-                    <div className="col-12 col-md m-1 ">
-                        <Card  border="primary">
-                            <CardImg top width="100%" height="300px" src="assets/images/img1.jpg" alt="Home Card" />
-                            <CardBody>
-                                <CardTitle tag="h5">Random Post #1</CardTitle>
-                                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                                    <img src="assets/images/user.png" name="hasintha" alt="hasintha" width="24px" height="24px"/>   Writtern by: User Random #1
-                                </CardSubtitle>
-                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</CardText>
-                                <Button>Read More!</Button>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md m-1">
-                        <Card>
-                            <CardImg top width="100%" height="300px" src="assets/images/img2.jpg" alt="Home Card" />
-                            <CardBody>
-                                <CardTitle tag="h5">Random Post #2</CardTitle>
-                                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                                    <img src="assets/images/user.png" name="hasintha" alt="hasintha" width="24px" height="24px"/>   Writtern by: User Random #2
-                                </CardSubtitle>
-                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</CardText>
-                                <Button>Read More!</Button>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md m-1">
-                        <Card>
-                            <CardImg top width="100%" height="300px" src="/assets/images/img3.png" alt="Home Card" />
-                            <CardBody>
-                                <CardTitle tag="h5">Random Post #3</CardTitle>
-                                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                                    <img src="assets/images/user.png" name="hasintha" alt="hasintha" width="24px" height="24px"/>   Writtern by: User Random #3
-                                </CardSubtitle>
-                                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</CardText>
-                                <Button>Read More!</Button>
-                            </CardBody>
-                        </Card>
-                    </div>
-                </div>
-                <br />
+                
 
                 <div>
                     <div className="col-12 col-md m-1">
